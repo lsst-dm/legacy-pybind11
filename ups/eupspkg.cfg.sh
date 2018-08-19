4529e5f04ae132298393576b90b5b82eb61d3730
Tim Jenness <tjenness@lsst.org>
2018-08-19 10:32:05 -0700

    Explicitly say we want python from path
    I sometimes see pybind11 pick up /usr/local/bin/python even
    though the anaconda python is ahead of the path. This change
    to the eupspkg file forces it to use "python" from PATH.

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -2,7 +2,7 @@
 
 config()
 {
-    (rm -rf build && mkdir build && cd build && cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..)
+    (rm -rf build && mkdir build && cd build && cmake -DPYTHON_EXECUTABLE:FILEPATH=$(command -v python) -DCMAKE_INSTALL_PREFIX=$PREFIX ..)
 }
 
 build()
